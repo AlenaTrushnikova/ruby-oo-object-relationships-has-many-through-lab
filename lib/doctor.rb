4aa9fbb49916e418e95fdcecd15c2d86ae7796f3
AlenaTrushnikova <alena.yu.t@gmail.com>
2020-11-23 11:42:20 -0800

    done

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
+require 'pry'
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select{|appt| appt.doctor == self}
+    end
+    
+    def patients
+        self.appointments.map do |appt| 
+            appt.patient
+        end
+    end
+end
